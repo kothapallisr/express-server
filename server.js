86e279a1b7809ea837f423bf064a2ce814777dce
Sridhar Kothapalli <sridr8@gmail.com>
2020-07-14 19:18:51 +0530

    routes

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.get("/",function(req, res) {
+  res.send("<h1>Hello World!</h1>");
+})
+
+app.get("/contact", function(req, res) {
+  res.send("Contact me at sriurs@saimail.com");
+})
+
+app.get("/hobbies", function(req, res) {
+  res.send("<ul><li>Badminton</li><li>Carroms</li><li>Cooking</li></ul>")
+})
+
 app.listen(3000, function() {
   console.log("Server started on port: 3000");
 })
